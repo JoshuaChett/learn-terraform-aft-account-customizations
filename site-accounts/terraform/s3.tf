738f3fc6bc41ef4d55aeec25c68196ac2eb4c449
JoshuaChett <joshuachett@gmail.com>
2024-03-26 08:55:48 -0400

    creating new accounts

---
+++ site-accounts/terraform/s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "long_term_data_store" {
+  bucket = "Archive"
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.long_term_data_store.id
+  acl    = "private"
+}
+
+
+
+
+
+#An IAM user with full access to S3 - Unless this should be configured through the root identity center
+#I think it may actually make sense to have a bucket per data source, 
