259b37c9939afb9d5ad9edd673143927b9afa62e
JoshuaChett <joshuachett@gmail.com>
2024-03-27 16:17:07 -0400

    making terraform use a more recent version

--- sandbox/terraform/versions.tf
+++ sandbox/terraform/versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = ">= 5.8.0"
     }
   }
+  required_version = ">= 1.1.5"
 }
